5849656ca641e38c6ca6402566021105a2dfc05c
jacoposergio <djago32@gmail.com>
2022-10-27 11:13:18 +0200

    fixed

--- Program.cs
+++ Program.cs
@@ -1,30 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+//array con cibi
 string[] CibiPreferiti = { "Carne", "Pizza", "Hamburger", "Pasta", "Gelato", "Kebab", "Patatine"};
-
 
 //lunghezza lista
 Console.WriteLine("lunghezza lista: " + CibiPreferiti.Length);
 
-//stampo tutti i cibi
+//stampo la classifica di tutti i cibi
 Console.WriteLine("Classifica migliori cibi:");
+
 for (int i = 0; i < CibiPreferiti.Length; i++ )
 {
     Console.WriteLine((i + 1) + "°" + "" + "posto:" + "" + CibiPreferiti[i]);
 };
 
-//Console.WriteLine("Classifica migliori cibi");
-//Console.WriteLine("primo posto: " + CibiPreferiti[0]);
-//Console.WriteLine("secondo posto: " + CibiPreferiti[1]);
-//Console.WriteLine("terzo posto: " + CibiPreferiti[2]);
-//Console.WriteLine("quarto posto: " + CibiPreferiti[3]);
-//Console.WriteLine("quinto posto: " + CibiPreferiti[4]);
-//Console.WriteLine("sesto posto: " + CibiPreferiti[5]);
-//Console.WriteLine("settimo posto: " + CibiPreferiti[6]);
-
+//migliore e peggiore
 Console.WriteLine("cibo top: " + CibiPreferiti[0]);
 Console.WriteLine("cibo meno top: " + CibiPreferiti[6]);
 
+//medio
 Console.WriteLine("cibo mezza classifica pari: " + CibiPreferiti[CibiPreferiti.Length / 2]);
 Console.WriteLine("cibo mezza classifica dispari: " + CibiPreferiti[(CibiPreferiti.Length / 2) + 1]);
